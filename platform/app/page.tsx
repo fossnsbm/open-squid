0dc9a8437179cd2fc0fd30ffc8266757e2dc8874
libresoul <contact@damindu.me>
2025-08-04 04:50:06 +0530

    adds RippleGrid

--- platform/app/page.tsx
+++ platform/app/page.tsx
@@ -1,7 +1,10 @@
 import Header from "./components/common/Header";
+import RippleGrid from "./components/landing/RippleGrid";
+
 export default function Home() {
     return (
         <>
+            <RippleGrid />
             <Header />
         </>
     );
