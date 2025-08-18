a4eebb08590b805305b3edc7a27678746ff34761
libresoul <contact@damindu.me>
2025-08-18 23:01:19 +0530

    chore: format code

--- platform/app/playground/page.tsx
+++ platform/app/playground/page.tsx
@@ -5,77 +5,76 @@ import Toast from "../components/common/Toast";
 import QuizSurvival from "../components/playground/QuizSurvival";
 export default function GameSelection() {
 
-  const [showToast, setShowToast] = useState<{
-    message: string;
-    type: "success" | "error";
-  } | null>(null);
+    const [showToast, setShowToast] = useState<{
+        message: string;
+        type: "success" | "error";
+    } | null>(null);
 
-  const [activeGame, setActiveGame] = useState<"quiz survival" | "echo prompt" | "squid clue">("quiz survival");
-  const games = ["quiz survival", "echo prompt", "squid clue"] as const;
+    const [activeGame, setActiveGame] = useState<"quiz survival" | "echo prompt" | "squid clue">("quiz survival");
+    const games = ["quiz survival", "echo prompt", "squid clue"] as const;
 
-  return (
-    <div>
+    return (
+        <div>
 
-          {showToast && (
+            {showToast && (
                 <Toast
                     message={showToast.message}
                     type={showToast.type}
                     onClose={() => setShowToast(null)}
                 />
             )}
-        
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 py-10 px-4 font-squid uppercase">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold text-center text-white mb-10">
-         PlayGround
-        </h1>
 
-       
-        <div className="relative flex justify-center mb-8 ">
-          <div className="flex bg-gray-800 rounded-full shadow-md p-1 relative">
-            {games.map((game) => (
-              <button
-                key={game}
-                onClick={() => setActiveGame(game)}
-                className={`relative z-10 px-6 py-2 rounded-full font-medium transition-all duration-300  uppercase text-sm md:text-lg
-                  ${
-                    activeGame === game
-                      ? "text-white"
-                      : "text-gray-600 hover:text-pink-600"
-                  }
+            <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 py-10 px-4 font-squid uppercase">
+                <div className="max-w-4xl mx-auto">
+                    <h1 className="text-4xl font-bold text-center text-white mb-10">
+                        PlayGround
+                    </h1>
+
+
+                    <div className="relative flex justify-center mb-8 ">
+                        <div className="flex bg-gray-800 rounded-full shadow-md p-1 relative">
+                            {games.map((game) => (
+                                <button
+                                    key={game}
+                                    onClick={() => setActiveGame(game)}
+                                    className={`relative z-10 px-6 py-2 rounded-full font-medium transition-all duration-300  uppercase text-sm md:text-lg
+                  ${activeGame === game
+                                            ? "text-white"
+                                            : "text-gray-600 hover:text-pink-600"
+                                        }
                 `}
-              >
-                {game}
-              </button>
-            ))}
+                                >
+                                    {game}
+                                </button>
+                            ))}
 
-            
-            <div
-              className="absolute top-1 bottom-1 left-1 bg-pink-600 rounded-full shadow-xl transition-all duration-300 "
-              style={{
-                width: `${100 / games.length}%`,
-                transform: `translateX(${games.indexOf(activeGame) * 100}%)`,
-              }}
-            ></div>
-          </div>
+
+                            <div
+                                className="absolute top-1 bottom-1 left-1 bg-pink-600 rounded-full shadow-xl transition-all duration-300 "
+                                style={{
+                                    width: `${100 / games.length}%`,
+                                    transform: `translateX(${games.indexOf(activeGame) * 100}%)`,
+                                }}
+                            ></div>
+                        </div>
+                    </div>
+
+                    <div className="boarder-1 bg-pink-700 rounded-xl shadow-lg p-0.5 transition-all duration-500">
+                        {activeGame === "quiz survival" && <QuizSurvival />}
+                        {activeGame === "echo prompt" && <EchoPrompt />}
+                        {activeGame === "squid clue" && <Game3Section />}
+                    </div>
+                </div>
+            </div>
         </div>
-    
-        <div className="boarder-1 bg-pink-700 rounded-xl shadow-lg p-0.5 transition-all duration-500">
-          {activeGame === "quiz survival" && <QuizSurvival />}
-          {activeGame === "echo prompt" && <EchoPrompt />}
-          {activeGame === "squid clue" && <Game3Section />}
-        </div>
-      </div>
-    </div>
-    </div>
-  );
+    );
 }
 
 
 function Game1Section() {
-  return <p className="text-gray-700">Game 1 content...</p>;
+    return <p className="text-gray-700">Game 1 content...</p>;
 }
 
 function Game3Section() {
-  return <p className="text-gray-700">Game 3 content...</p>;
+    return <p className="text-gray-700">Game 3 content...</p>;
 }
